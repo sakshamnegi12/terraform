739a7f63b7844b8f32a3af1785f743f54c012436
root <root@DESKTOP-HAF822D.localdomain>
2026-05-27 02:17:43 +0530

    remote lock management

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,12 @@ terraform {
       version = "6.28.0"
     }
   }
+
+bucket "s3" {
+bucket = "terraformstatebucket-tws"
+key = "terraform.tfstate"
+dynamodb_table = "terraform-state-table"
+region = "ap-south-1"
+ }
 }
+
